5f9ab1db66e2ba3b5ed651c9876d8b043c539df4
valentin7 <valpe70@gmail.com>
2014-01-24 21:04:18 -0500

    initial configuration

--- Connect/AppDelegate.m
+++ Connect/AppDelegate.m
@@ -12,10 +12,11 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    /*self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
-    [self.window makeKeyAndVisible];
+    [self.window makeKeyAndVisible];*/
+    NSLog(@"something");
     return YES;
 }
 
