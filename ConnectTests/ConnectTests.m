48acaa062f8f136a1eb349d53374c9c98a63229d
valentin7 <valpe70@gmail.com>
2014-01-24 20:24:12 -0500

    Initial Commit

---
+++ ConnectTests/ConnectTests.m
@@ -0,0 +1,34 @@
+//
+//  ConnectTests.m
+//  ConnectTests
+//
+//  Created by Valentin Perez on 1/24/14.
+//  Copyright (c) 2014 ConnectHack. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ConnectTests : XCTestCase
+
+@end
+
+@implementation ConnectTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
